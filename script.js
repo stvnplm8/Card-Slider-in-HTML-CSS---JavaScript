ad817ae27b2b5bb73e8b1220395061ef80a6f705
Steven <stvnplm8@gmail.com>
2024-09-24 15:00:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+new Swiper(".card-wrapper", {
+  loop: true,
+  spaceBetween: 30, // Use a colon instead of a semicolon
+
+  // Pagination Bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive Breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
